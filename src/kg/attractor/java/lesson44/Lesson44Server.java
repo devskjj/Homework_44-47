c52052372579b3fc6ce22f2ae107cbf53d8d4b62
Denis Eroshevskii <devskjj@gmail.com>
2025-11-22 22:14:06 +0600

    "Подготовил проект"

---
+++ src/kg/attractor/java/lesson44/Lesson44Server.java
@@ -0,0 +1,61 @@
+package kg.attractor.java.lesson44;
+
+import com.sun.net.httpserver.HttpExchange;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+import kg.attractor.java.server.BasicServer;
+import kg.attractor.java.server.ContentType;
+import kg.attractor.java.server.ResponseCodes;
+
+import java.io.*;
+
+public class Lesson44Server extends BasicServer {
+    private final static Configuration freemarker = initFreeMarker();
+
+    public Lesson44Server(String host, int port) throws IOException {
+        super(host, port);
+        registerGet("/sample", this::freemarkerSampleHandler);
+    }
+
+    private static Configuration initFreeMarker() {
+        try {
+            Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+            cfg.setDirectoryForTemplateLoading(new File("data"));
+            cfg.setDefaultEncoding("UTF-8");
+            cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+            cfg.setLogTemplateExceptions(false);
+            cfg.setWrapUncheckedExceptions(true);
+            cfg.setFallbackOnNullLoopVariable(false);
+            return cfg;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void freemarkerSampleHandler(HttpExchange exchange) {
+        renderTemplate(exchange, "sample.html", getSampleDataModel());
+    }
+
+    protected void renderTemplate(HttpExchange exchange, String templateFile, Object dataModel) {
+        try {
+            Template temp = freemarker.getTemplate(templateFile);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+            try (OutputStreamWriter writer = new OutputStreamWriter(stream)) {
+                temp.process(dataModel, writer);
+                writer.flush();
+
+                var data = stream.toByteArray();
+                sendByteData(exchange, ResponseCodes.OK, ContentType.TEXT_HTML, data);
+            }
+        } catch (IOException | TemplateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private SampleDataModel getSampleDataModel() {
+        return new SampleDataModel();
+    }
+}
