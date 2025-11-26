23ea3d6a9ee217b4c4cd3f6c96a3d420e53f42ba
Denis Eroshevskii <devskjj@gmail.com>
2025-11-27 03:53:49 +0600

    "task_1 Сделал обработку ошибки через редирект метод и булевый флаг error, добавил всплывающее окно."

--- src/kg/attractor/java/lesson44/Lesson45Server.java
+++ src/kg/attractor/java/lesson44/Lesson45Server.java
@@ -9,7 +9,7 @@ import kg.attractor.java.server.ResponseCodes;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Lesson45Server extends Lesson44Server {
@@ -47,6 +47,7 @@ public class Lesson45Server extends Lesson44Server {
         String cType = getContentType(exchange);
         String raw = getRequestBody(exchange);
 
+
         Map<String, String> parsed = Utils.parseUrlEncoded(raw, "&");
 
         String fmt = "<p>Необработанные данные: <b>%s</b></p>"
@@ -54,20 +55,13 @@ public class Lesson45Server extends Lesson44Server {
                 + "<p>После обработки: <b>%s</b></p>";
 
         String data = String.format(fmt, raw, cType, parsed);
-        String error = "Ошибка регистрация не удалась, т.к. данный email уже занят";
-
 
         boolean isAlreadyExists = dataModel.getUsers().stream()
                 .filter(u -> u.getEmail() != null)
                 .anyMatch(e -> e.getEmail().equalsIgnoreCase(parsed.get("user-email")));
 
         if (isAlreadyExists) {
-            try {
-                sendByteData(exchange, ResponseCodes.OK, ContentType.TEXT_HTML, error.getBytes());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+            redirect303(exchange, "/register?error=1");
         } else {
             try {
                 addNewUserToJson(parsed);
@@ -79,6 +73,16 @@ public class Lesson45Server extends Lesson44Server {
         }
 
 
+    }
+
+    protected void redirect303(HttpExchange exchange, String path) {
+        try {
+            exchange.getResponseHeaders().add("Location", path);
+            exchange.sendResponseHeaders(303, 0);
+            exchange.getResponseBody().close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void addNewUserToJson(Map<String, String> parsed) {
@@ -97,8 +101,23 @@ public class Lesson45Server extends Lesson44Server {
 
     private void registerHandler(HttpExchange exchange) {
         Path path = makeFilePath("register.html");
-        sendFile(exchange, path, ContentType.TEXT_HTML);
 
+        Map<String, Object> templateModel = getErrorStatusFromQuery(exchange);
+        renderTemplate(exchange, "register.html", templateModel);
+    }
+
+    private Map<String, Object> getErrorStatusFromQuery(HttpExchange exchange) {
+        String query = exchange.getRequestURI().getQuery();
+        boolean error = false;
+
+        if (query != null) {
+            Map<String, String> queryMap = Utils.parseUrlEncoded(query, "&");
+            error = "1".equals(queryMap.get("error"));
+        }
+
+        Map<String, Object> templateModel = new HashMap<>();
+        templateModel.put("error", error);
+        return templateModel;
     }
 
 
