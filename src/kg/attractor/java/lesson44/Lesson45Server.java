575b135bc392e1a9c0dae915a2b6806af6e72045
Denis Eroshevskii <devskjj@gmail.com>
2025-11-28 00:33:48 +0600

    "task_2 Добавил обработки путей profile для запроса напрямую и отдельный для пользователя. Сделал обработку индентификатора это email для входа, через стримы с последующим редиректом. Сделал проверку и отображение профиля для пользователя по его id."

--- src/kg/attractor/java/lesson44/Lesson45Server.java
+++ src/kg/attractor/java/lesson44/Lesson45Server.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collector;
 
 public class Lesson45Server extends Lesson44Server {
     public Lesson45Server(String host, int port, SampleDataModel dataModel) throws IOException {
@@ -20,8 +22,11 @@ public class Lesson45Server extends Lesson44Server {
         registerPost("/login", this::loginPostHandler);
         registerGet("/register", this::registerHandler);
         registerPost("/register", this::registerPostHandler);
-
+        registerGet("/profile", this::profileHandler);
+        registerGet("/profile/user", this::profileUserHandler);
     }
+
+
 
 
     private void loginPostHandler(HttpExchange exchange) {
@@ -36,25 +41,39 @@ public class Lesson45Server extends Lesson44Server {
 
         String data = String.format(fmt, raw, cType, parsed);
 
-        try {
-            sendByteData(exchange, ResponseCodes.OK, ContentType.TEXT_HTML, data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
+        boolean isMatch = dataModel.getUsers().stream()
+                .filter(u -> u.getEmail() != null)
+                .filter(u -> u.getPassword() != null && u.getPassword().equals(parsed.get("user-password")))
+                .anyMatch(u -> u.getEmail().equalsIgnoreCase(parsed.get("user-email")));
+
+        if (isMatch) {
+            var userId = dataModel.getUsers().stream()
+                    .filter(u -> u.getEmail() != null)
+                    .filter(u -> u.getEmail().equalsIgnoreCase(parsed.get("user-email")))
+                    .map(User::getId)
+                    .findFirst()
+                    .orElse(-1);
+
+            System.out.println(userId);
+            redirect303(exchange, "/profile/user?id=" + userId);
+//            renderTemplate(exchange, "profile.html", templateModel);
+//            redirect303(exchange, redirectUrl);
+        } else {
+            Map<String, Object> templateModel = new HashMap<>();
+            templateModel.put("error", true);
+            renderTemplate(exchange, "login.html", templateModel);
+
         }
+
+
+
     }
 
     private void registerPostHandler(HttpExchange exchange) {
         String cType = getContentType(exchange);
         String raw = getRequestBody(exchange);
 
-
         Map<String, String> parsed = Utils.parseUrlEncoded(raw, "&");
-
-        String fmt = "<p>Необработанные данные: <b>%s</b></p>"
-                + "<p>Content-type: <b>%s</b></p>"
-                + "<p>После обработки: <b>%s</b></p>";
-
-        String data = String.format(fmt, raw, cType, parsed);
 
         boolean isAlreadyExists = dataModel.getUsers().stream()
                 .filter(u -> u.getEmail() != null)
@@ -91,7 +110,7 @@ public class Lesson45Server extends Lesson44Server {
     }
 
     private void addNewUserToJson(Map<String, String> parsed) {
-        int newId = dataModel.getUsers().size() + 1;
+        var newId = dataModel.getUsers().size() + 1;
         dataModel.getUsers().add(new User(
                 parsed.get("user-name"),
                 parsed.get("user-email"),
@@ -101,7 +120,7 @@ public class Lesson45Server extends Lesson44Server {
 
     private void loginHandler(HttpExchange exchange) {
         Path path = makeFilePath("login.html");
-        sendFile(exchange, path, ContentType.TEXT_HTML);
+        renderTemplate(exchange, "login.html", path);
     }
 
     private void registerHandler(HttpExchange exchange) {
@@ -109,19 +128,38 @@ public class Lesson45Server extends Lesson44Server {
         renderTemplate(exchange, "register.html", path);
     }
 
-//    private Map<String, Object> getErrorStatusFromQuery(HttpExchange exchange) {
-//        String query = exchange.getRequestURI().getQuery();
-//        boolean error = false;
-//
-//        if (query != null) {
-//            Map<String, String> queryMap = Utils.parseUrlEncoded(query, "&");
-//            error = "1".equals(queryMap.get("error"));
-//        }
-//
-//        Map<String, Object> templateModel = new HashMap<>();
-//        templateModel.put("error", error);
-//        return templateModel;
-//    }
+    private void profileHandler(HttpExchange exchange) {
+        Path path = makeFilePath("profile.html");
+        renderTemplate(exchange, "profile.html", path);
+    }
 
+    private void profileUserHandler(HttpExchange exchange) {
+        String query = exchange.getRequestURI().getQuery();
+        if (query == null || query.isEmpty()) {
+            respond404(exchange);
+            return;
+        }
 
+        Map<String, String> queryParams = Utils.parseUrlEncoded(query, "&");
+        String userIdParam = queryParams.get("id");
+        if (userIdParam == null) {
+            respond404(exchange);
+            return;
+        }
+
+        try {
+            var userId = Integer.parseInt(userIdParam);
+            var user = dataModel.getUserById(userId);
+
+            if (user != null) {
+                Map<String, Object> templateModel = new HashMap<>();
+                templateModel.put("user", user);
+                renderTemplate(exchange, "profile.html", templateModel);
+            } else {
+                respond404(exchange);
+            }
+        } catch (NumberFormatException e) {
+            respond404(exchange);
+        }
+    }
 }
